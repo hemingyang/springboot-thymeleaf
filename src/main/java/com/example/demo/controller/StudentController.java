d1465a3cf3b52d3180e98cbe64a48aa2cc371de2
hemingyang <hemingyangyang@gmail.com>
2020-09-12 18:44:12 +0800

    增加 Models

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -8,14 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 //@Api(value="students",tags = "students")
 @Api(tags ="学生",value = "students" )
-@Controller
+@RestController
 @RequestMapping("/students/")
 public class StudentController {
 
@@ -40,7 +37,7 @@ public class StudentController {
     }
     @ApiOperation(tags = "添加",value = "add")
     @PostMapping("add")
-    public String addStudent(@Valid Student student, BindingResult result, Model model) {
+    public String addStudent(@Valid  @RequestBody Student student,  BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "add-student";
         }
@@ -58,7 +55,7 @@ public class StudentController {
     }
     @ApiOperation(tags = "跟新",value = "edit")
     @PostMapping("update/{id}")
-    public String updateStudent(@PathVariable("id") long id, @Valid Student student, BindingResult result,
+    public String updateStudent(@PathVariable("id") long id, @Valid  @RequestBody Student student, BindingResult result,
                                 Model model) {
         if (result.hasErrors()) {
             student.setId(id);
--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,27 +1,38 @@
 package com.example.demo.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Entity
+@ApiModel("学生")
 @Table(name = "Student")
 public class Student implements Serializable {
 
     @Id
+    @ApiModelProperty("id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @ApiModelProperty("name")
     @NotBlank(message = "Name is 姓名")
     @Column(name = "name")
     private String name;
 
+    @ApiModelProperty("email")
     @NotBlank(message = "Email")
     @Column(name = "email")
     private String email;
 
+    @ApiModelProperty("phone_no")
+    @NotBlank(message = "phone_no")
     @Column(name = "phone_no")
     private long phoneNo;
 
+    @ApiModelProperty("code")
     @NotBlank(message = "学号")
     @Column(name="code")
     private  String  code;
